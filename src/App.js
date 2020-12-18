f7130f33f011eab13faeec0297fc7bdc97c18735
Sanyia Saidova <sanyia.saidova@gmail.com>
2020-12-18 18:29:43 +0000

    Adding more navigation fields, footer and updating look of details section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import SubscribeForm from './components/SubscribeForm'
 import InlineAnchor from './components/InlineAnchor'
+import SignUpBtn from './components/SignUpBtn'
 import theme from './theme'
+import { Info, Contact, Favorite } from 'grommet-icons'
 import {
   Header,
   Box,
@@ -12,7 +14,9 @@ import {
   Carousel, 
   Image, 
   Heading, 
-  Text
+  Text,
+  Footer,
+  Tip
 } from 'grommet'
 
 
@@ -21,8 +25,17 @@ export default function App() {
     <Grommet theme={theme} background='brand-1' full={true}>
       <Header>
         <Box background={{'image': 'url(/site-logo.png)', 'size': 'contain', 'opacity': 'medium'}} height='60px' width='70px' margin='30px 0 0 30px'/>
-        <Nav direction="row-reverse" pad="medium">
+        <Nav direction="row-reverse" pad="medium" align='center'>
+          <Tip content='Coming soon!'>
+            <SignUpBtn />
+          </Tip>
           <Anchor label='Contact' href='#contact' hoverIndicator />
+          <Tip content='Coming soon!'>
+            <Anchor label='Explore Bundles' href='#' hoverIndicator />
+          </Tip>
+          <Tip content='Coming soon!'>
+            <Anchor label='Blog' href='#' hoverIndicator />
+          </Tip>
           <Anchor label='What is this?' href='#about' hoverIndicator />
         </Nav>
       </Header>
@@ -50,21 +63,26 @@ export default function App() {
             <Image fit="contain" src="/screenshots/admin__client_admin-settings_bundles_view-bundle__mac-edu.png" />
           </Carousel>
         </Box>
-        <Box gridArea="about" id="about" pad="medium" background={{'color': 'white', 'opacity': 'weak'}}>
+        <Box gridArea="about" id="about" align='center' pad="medium" background={{'color': 'white', 'opacity': 'weak'}}>
+          <Info size='large' color='accent-1'/>
           <Heading textAlign='center'>What is this?</Heading>
-          <Text textAlign='center'>
+          <Text textAlign='center' pad='large'>
             Appseam is a desktop application that helps teams manage their tool stack.
             Our aim is to provide an interface where you can manage tool installations and setups per team using bundles. 
             Instead of asking your team members to go through a checklist of activities to get them setup, you can use Appseam to automate these activities for you.
           </Text>
         </Box>
-        <Box gridArea="contact" id="contact" pad="medium">
+        <Box gridArea="contact" id="contact" pad="medium" align='center'>
+          <Contact size='large' color='accent-1'/>
           <Heading textAlign='center'>Contact Us</Heading>
-          <Text textAlign='center'>
+          <Text textAlign='center' pad='large'>
             We're still trying to get things setup, but if your team has issues managing your team members' access and setup, or processing requests for new tools, we'd love to hear from you at <InlineAnchor href='mailto:general@appseam.com'>general@appseam.com</InlineAnchor>. Please get in touch letting us know of the biggest problem you're facing regarding business tool provisioning and what you'd like to see. We'll aim to get in touch within 24 hours to see how we can solve your problem.
           </Text>
         </Box>
-      </Grid>     
+      </Grid>
+      <Footer size='small' pad='large' align='center' justify='center' background={{'color': 'black', 'opacity': 'weak'}}>
+        Made with <Favorite color='#DE2B37'/> by Appseam
+      </Footer>
     </Grommet>
   );
 }
