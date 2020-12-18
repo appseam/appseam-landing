e8694a0ce630d72544924ccebc81f4f3bd464b25
Sanyia Saidova <sanyia.saidova@gmail.com>
2020-12-18 19:57:27 +0000

    Updating theme colours

--- src/theme.js
+++ src/theme.js
@@ -6,14 +6,17 @@ const theme = {
         "light": '#272A27',
       },
       'brand-1': '#282343',
+      'brand-2': '#9D4EDD',
+      'brand-3': '#B376E5',
       'accent-1': '#3BFF76',
+      'accent-2': '#85FFA9',
       'grey-1': '#272A27',
       'white': '#FFFFFF',
       'danger-1': '#DE2B37'
     },
     control: {
       border: {
-        radius: '50px',
+        radius: '10px',
         width: '2px',
         color: 'white'
       }
@@ -27,13 +30,58 @@ const theme = {
   grommet: {
     extend: `{background-image: linear-gradient(#282343, #36305b, #443c72, #6054a0)}`
   },
+  layer: {
+    background: {
+      light: 'grey-1',
+      dark: 'brand-1'
+    }
+  },
   button: {
-    color: 'brand-1',
+    transition: {
+      timing: 'ease-in-out',
+      duration: '0.3'
+    },
+    default: {
+      color: 'white',
+    },
     border: {
-      color: 'accent-1'
+      radius: '10px',
     },
     primary: {
-      color: 'accent-1'
+      font: {
+        weight: 'bold'
+      },
+      border: {
+        color: 'accent-1'
+      },
+      background: {
+        color: 'accent-1'
+      },
+      color: 'brand-1'
+    },
+    secondary: {
+      background: {
+        color: 'brand-2'
+      },
+      color: 'white'
+    },
+    hover: {
+      default: {
+        background: {
+          color: 'white',
+          opacity: 0.2
+        }
+      },
+      primary: {
+        background: {
+          color: 'accent-2'
+        }
+      },
+      secondary: {
+        background: {
+          color: 'brand-3'
+        }
+      }
     }
   },
   anchor: {
